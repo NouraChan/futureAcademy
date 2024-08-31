d7f32b13b03c68b79bedbb56efe713bb1c7a8593
norhanali <162762157+NouraChan@users.noreply.github.com>
2024-08-31 15:28:46 +0300

    Update CoursesController.php

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -12,7 +12,7 @@ class CoursesController extends Controller
      */
     public function index()
     {
-        //
+        return view ('courses.index');
     }
 
     /**
@@ -20,7 +20,7 @@ class CoursesController extends Controller
      */
     public function create()
     {
-        //
+        return view ('courses.create');
     }
 
     /**
